1ecdf31ddb28c116ae522006939246d0bb75a22c
ivy forever <ivy@ivy.rs>
2026-07-22 01:10:16 +0100

    revert aerospace keybindings from ctrl-alt back to ctrl

--- modules/darwin/aerospace.nix
+++ modules/darwin/aerospace.nix
@@ -16,32 +16,32 @@
         default-root-container-orientation = "vertical";
 
         mode.main.binding = {
-          ctrl-alt-h = "focus left";
-          ctrl-alt-j = "focus down";
-          ctrl-alt-k = "focus up";
-          ctrl-alt-l = "focus right";
+          ctrl-h = "focus left";
+          ctrl-j = "focus down";
+          ctrl-k = "focus up";
+          ctrl-l = "focus right";
 
-          ctrl-alt-shift-h = "move left";
-          ctrl-alt-shift-j = "move down";
-          ctrl-alt-shift-k = "move up";
-          ctrl-alt-shift-l = "move right";
+          ctrl-shift-h = "move left";
+          ctrl-shift-j = "move down";
+          ctrl-shift-k = "move up";
+          ctrl-shift-l = "move right";
 
-          ctrl-alt-slash = "layout tiles horizontal vertical";
-          ctrl-alt-comma = "layout accordion horizontal vertical";
+          ctrl-slash = "layout tiles horizontal vertical";
+          ctrl-comma = "layout accordion horizontal vertical";
 
-          ctrl-alt-1 = "workspace 1";
-          ctrl-alt-2 = "workspace 2";
-          ctrl-alt-3 = "workspace 3";
-          ctrl-alt-4 = "workspace 4";
-          ctrl-alt-5 = "workspace 5";
+          ctrl-1 = "workspace 1";
+          ctrl-2 = "workspace 2";
+          ctrl-3 = "workspace 3";
+          ctrl-4 = "workspace 4";
+          ctrl-5 = "workspace 5";
 
-          ctrl-alt-shift-1 = "move-node-to-workspace 1";
-          ctrl-alt-shift-2 = "move-node-to-workspace 2";
-          ctrl-alt-shift-3 = "move-node-to-workspace 3";
-          ctrl-alt-shift-4 = "move-node-to-workspace 4";
-          ctrl-alt-shift-5 = "move-node-to-workspace 5";
+          ctrl-shift-1 = "move-node-to-workspace 1";
+          ctrl-shift-2 = "move-node-to-workspace 2";
+          ctrl-shift-3 = "move-node-to-workspace 3";
+          ctrl-shift-4 = "move-node-to-workspace 4";
+          ctrl-shift-5 = "move-node-to-workspace 5";
 
-          ctrl-alt-shift-semicolon = "mode service";
+          ctrl-shift-semicolon = "mode service";
         };
 
         mode.service.binding = {
