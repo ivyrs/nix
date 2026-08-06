852ed46c52b3585f2e44e49faa0ef14feb31bdd4
ivy forever <ivy@ivy.rs>
2026-08-06 23:54:57 +0100

    [pi] Done! Updated the Emacs font to "Aporetic Sans (mo...

--- modules/aspects/dev/emacs.nix
+++ modules/aspects/dev/emacs.nix
@@ -51,6 +51,11 @@
         (menu-bar-mode -1)
         (scroll-bar-mode -1)
         
+        ;; Font configuration
+        (set-face-attribute 'default nil :font "Aporetic Sans (mono)" :height 110)
+        (set-face-attribute 'fixed-pitch nil :font "Aporetic Sans (mono)")
+        (set-face-attribute 'variable-pitch nil :font "Aporetic Sans (mono)")
+        
         ;; Enable line numbers
         (global-display-line-numbers-mode 1)
         
